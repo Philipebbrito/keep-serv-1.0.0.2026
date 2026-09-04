673c85c21616d81dcdf1ed0139c6207abcbdc601
Philipe Borges <philipe.borges@gmail.com>
2026-09-04 20:01:11 +0000

    MAÇÃ

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,14 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
   tanstackStart: {
-    // Redireciona a entrada do servidor incluída no TanStack Start para src/server.ts
-    servidor: { entrada: "servidor" },
+    server: { entry: "server" },
   },
   vite: {
     server: {
       host: "0.0.0.0",
       port: 8080,
-      strictPort: true,
-      allowedHosts: [".app.github.dev"],
+      strictPort: false,
+      allowedHosts: true,
     },
   },
 });
